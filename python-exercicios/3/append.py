0cad22baed92f45ffc099dc6d6ab8e9b3396c40e
RafaelFino <rafael.gottardi@gmail.com>
2025-11-10 21:30:45 -0300

    Format

--- python-exercicios/3/append.py
+++ python-exercicios/3/append.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 file = input(">> Digite o nome do arquivo para adicionar conteúdo: ")
 with open(file, "a") as f:
     content = input(">> Digite o conteúdo para adicionar ao arquivo: ")
